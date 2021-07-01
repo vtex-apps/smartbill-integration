bb540cde7c5150e6955acc25dc1f61fda88333cd
Dan Nedelcu <dan.nedelcu@customsoft.ro>
2021-07-01 15:35:20 +0300

    ref #54380

---
+++ node/settings.tsx
@@ -0,0 +1,8 @@
+export default {
+    constants: {
+        smartbillEndpoint: 'http://ws.smartbill.ro/SBORO/api',
+        country: 'Romania',
+        measuringUnitName: 'buc',
+        taxName: 'Normala'
+    }
+}
